84875b7c14c5090114f9068e728b37406cf71ede
Kazuki Oishi <oishikazuki@gmail.com>
2017-04-18 17:32:06 +0900

    add some files.

---
+++ README.md
@@ -0,0 +1,17 @@
+# pixiv_bookmark
+モバイル版pixivの様にワンクリックでブックマークできるようにするUserscript。
+
+## Install
+<https://greasyfork.org/ja/scripts/26625-pixiveasybookmark>
+
+からどうぞ
+
+## Build
+注:Windowsをお使いの場合はBash on Windowsで実行してください。多分動かないです。
+
+```
+$ npm install
+```
+```
+$ npm run build
+```
--- make.ts
+++ make.ts
@@ -1,10 +1,10 @@
 /// <reference path="./typings/index.d.ts"/>
 var fs = require('fs');
 var execSync: any = require('child_process').execSync;
-execSync('tsc', { stdio: [0, 1, 2] })
+execSync('$(npm bin)/tsc', { stdio: [0, 1, 2] })
 //script will be compiled to pixiv_bookmark.user.js
 var header: string = fs.readFileSync('./header.js', 'utf8');
 var script: string = fs.readFileSync('./out/pixiv_bookmark.user.js', 'utf8');
 var out: string = header + '\n' + script
 fs.writeFileSync('./out/pixiv_bookmark.js', out)
-console.info('👍Build OK')
+console.info('👍Build OK. See "./out/pixiv_bookmark.js".')
